31caa3261951c56d900de7ac4f53a4bde55721fe
Thomas <thomas@craftacademy.se>
2021-03-24 07:51:47 +0100

    small fixes and changes in client WS server address needs to be updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const wsServer = new WebSocket.Server({port: 8080})
 wsServer.on('connection', connection => {
   connection.send('<h2>Hello my remote ppl!</h2>')
   connection.on('message', message => {
-    const message = JSON.parse(message)
+    message = JSON.parse(message)
     wsServer.clients.forEach(function each(client) {
       client.send(JSON.stringify(message))
     })
